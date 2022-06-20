a5121990cce6422d7f4fefe9983829dff1a4f8cd
kucer0043 <kucer0043@gmail.com>
2022-06-20 17:04:01 +0300

    Мультиплеерная игра лабиринт испрален баг с отключением клиента

--- e_socket.py
+++ e_socket.py
@@ -24,7 +24,7 @@ def client(ip, port):
 conn_old = None
 addr_old = None
 class multiplayer_socket:
-    def __init__(self,ip,port,type:str,listen=2):
+    def __init__(self,ip,port,type:str,listen=2,action_on_disconnect=None):
         self.sock = sock
         self.type = type# 1 for server, 2 for client
         self.data = 'TEST'
@@ -34,6 +34,7 @@ class multiplayer_socket:
         self.ip = ip
         self.port = port
         self.listen = listen
+        self.action_on_disconnect = action_on_disconnect
         if self.type == "1":
             server(self.ip,self.port)
             self.sock.listen(self.listen)
@@ -57,7 +58,8 @@ class multiplayer_socket:
                 if prinim == 1:
                     self.data_input = self.sock.recv(1024).decode()
         except:
-            print('ALL USERS DISCONECTED')
+            '''print('ALL USERS DISCONECTED')
             print('WAIT NEW USERS')
-            self.conn_old, self.addr_old = self.sock.accept()
+            self.conn_old, self.addr_old = self.sock.accept()'''
+            self.action_on_disconnect()
         return self.data_input
